9c66e9c7939450829959a7556dfdca1fd2fb7775
NiXWai <1149062971@qq.com>
2023-03-09 20:01:17 +0800

    feat:change base

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import WindiCSS from 'vite-plugin-windicss';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), WindiCSS()],
+  base: './',
   resolve: {
     // 别名
     alias: { '@': resolve(__dirname, 'src') }
