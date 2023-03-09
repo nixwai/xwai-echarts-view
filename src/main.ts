1aa227306d49df18ed35e45bfebad3c42c0907c9
NiXWai <1149062971@qq.com>
2023-03-09 10:48:43 +0800

    fix:add windicss

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import './style/index.scss';
+import 'virtual:windi.css';
 import App from './App.vue';
 
 createApp(App).mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
+import WindiCSS from 'vite-plugin-windicss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), WindiCSS()],
   resolve: {
     // 别名
     alias: { '@': resolve(__dirname, 'src') }
